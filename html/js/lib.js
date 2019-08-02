e3cf9ddc13b5e30854b4c78731a037e2247a77ac
alengrg98 <alengeorgeshibu@cs.ajce.in>
2019-08-02 16:34:41 +0530

    second commit

--- html/js/lib.js
+++ html/js/lib.js
@@ -1,1 +1,44 @@
-111165
+$(document).ready(function(){             
+
+ $('#nv1').click(function(){
+        window.location.assign('login.html');
+    })
+
+    $('#nv2').click(function(){
+        window.location.assign('register.html');
+    })
+    
+
+    $("#cat li").click(function(){
+        var cat = $(this).text();
+        if(cat=="All")
+        {
+            cat='Books';
+        }
+        console.log(cat);
+        
+        $.ajax({                        
+            type:"GET",                  
+            url :"books.json",         
+            beforeSend : function(){
+                $("#load").show();
+            },
+            success : function(data){
+                $("#load").hide();              
+                var card="<div class='card-deck'>";
+                for (var j in data){
+                    if (cat == data[j].category || cat == 'Books')
+                    {
+                        card+="<div class='col-4 col-sm-4 col-md-4'> <div class='card'> <img class='card-img-top' src=" + data[j].urltoimage + " height='400px' width='325px'>";
+                        card+="<div class='card-body'> <h5 class='card-title'>"+ data[j].title +"</h5>";
+                        card+="<h6 class='card-subtitle mb-2 text-muted'>" + data[j].author +"</h6> ";
+                        card+="<p class='card-text text-justify'>"+ data[j].description + "</p> </div>";
+                        card+="<div class='card-text text-center'> <p>Price: "+ data[j].price+"</p><p>Genre: "+ data[j].category+"</p></div></div></div> <br>";
+                    }
+                }
+                card+="</div>";
+                $(".results").html(card);
+            }         
+        });
+    });
+});
---
+++ html/js/login.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    $('#nv1').click(function(){
+        window.location.assign('login.html');
+    })
+
+    $('#nv2').click(function(){
+        window.location.assign('register.html');
+    })
+
+    $('#nv3').click(function(){
+        
+        window.location.assign('lib.html');
+    })
+
+    $('#b2').click(function(){
+        window.location.assign('login.html');
+    })
+
+   // $("#cat li").click(function(){
+   //     window.location.assign('lib.html');
+   // })
+    /*function validation(tuser,tpass,callback)
+    {
+        if((tuser!="admin")||(tpass!="12345"))
+        {
+            alert("Incorrect Username or Password");
+        }
+        else
+        {
+            callback();
+        }
+    }
+    
+    $('#b1').click(function()
+    {
+        var user=$('#inp1').val();
+        var pass=$('#inp2').val();
+        validation(user,pass,()=>{
+            window.location.assign('main.html');
+        })
+        
+        
+    })*/
+});
